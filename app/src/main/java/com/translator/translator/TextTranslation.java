bbb8ae941cee39e7c4e3894ddb5562acc04f23d5
Isaiah Inocentes <isaiahpaulinocentes@gmail.com>
2018-08-06 23:56:12 +0800

    Updated the Layout for text translation

--- app/src/main/java/com/translator/translator/TextTranslation.java
+++ app/src/main/java/com/translator/translator/TextTranslation.java
@@ -3,11 +3,11 @@ package com.translator.translator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class TextTranslation extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_text_translation);
     }
 }
