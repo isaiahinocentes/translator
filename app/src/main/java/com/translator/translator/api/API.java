924dcf1f4ef85591fa7633b1d1ac5ff364d8e761
Isaiah Inocentes <isaiahpaulinocentes@gmail.com>
2018-08-24 03:34:16 +0800

    Sample Commit

--- app/src/main/java/com/translator/translator/api/API.java
+++ app/src/main/java/com/translator/translator/api/API.java
@@ -2,7 +2,7 @@ package com.translator.translator.api;
 
 public class API{
 
-    //Yandex Translate API
+    //Yandex Translate APIs
         public static final String yURL = "https://translate.yandex.net/api/v1.5/tr.json/translate?"; //Yandex
         public static final String yKey = "key=trnsl.1.1.20180806T180600Z.5d41060ee1dee3cd.460818f1319dce121769272399a405d23ba9c4d5&";//Yandex
         public static final String yAPI_URL_KEY = yURL + yKey;
